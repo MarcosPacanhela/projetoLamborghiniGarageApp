eb3bb37ddeb743e0b9cbd26fc2e85d3e115bba22
Marcos Pacanhela <marcosalexandrepacanhela71@gmail.com>
2024-09-18 00:44:39 -0300

    Implementação da api com axios, configuração da regra de negócio para manipulação dos dados da api, além de ajuste no layout

--- src/components/CardView/styles.ts
+++ src/components/CardView/styles.ts
@@ -7,5 +7,47 @@ export const styles = StyleSheet.create({
         height: '100%',
         overflow: 'hidden',
         borderRadius: 8
+    },
+    logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: 200,
+        resizeMode: 'contain'
+    },
+    imageLogo: {
+        flex: 1,
+        width: '65%',
+        resizeMode: 'contain'
+    },
+    carBrand: {
+        color: '#fff',
+        fontWeight: "400",
+        fontSize: 18,
+        fontStyle: 'italic'
+    },
+    carName: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    image: {
+        flex: 1,
+        marginBottom: 10,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+    },
+    priceLabelContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingBottom: 10,
+        alignItems: 'center',
+        marginTop: 10
+    },
+    priceLabel: {
+        color: '#fff',
+        fontSize: 22
     }
 })
--- src/views/GarageView/index.tsx
+++ src/views/GarageView/index.tsx
@@ -1,15 +1,18 @@
 import { View, Text, StatusBar } from "react-native";
-import {styles} from './styles'
-export default function GarageView(){
-    return(
+import { styles } from './styles'
+import CardView from "../../components/CardView";
+export default function GarageView() {
+    return (
         <View style={styles.container}>
             <View style={styles.cameraBox}>
                 <View style={styles.triangleCorner}></View>
                 <View style={[styles.triangleCorner, styles.topRight]}></View>
                 <View style={[styles.triangleCorner, styles.bottomLeft]}></View>
                 <View style={[styles.triangleCorner, styles.bottomRight]}></View>
+                <StatusBar barStyle='light-content' />
+                <CardView />
             </View>
         </View>
-        
+
     )
 }
